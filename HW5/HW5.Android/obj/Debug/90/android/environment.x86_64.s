fd980f1c29053226c31aabecd3bda3bfcd2b1aef
RogelioCordova <55772023+droozyson@users.noreply.github.com>
2020-03-24 12:24:43 -0700

    Added initial map which can scroll and zoom, it is street view, essentially boiler plate

--- HW5/HW5.Android/obj/Debug/90/android/environment.x86_64.s
+++ HW5/HW5.Android/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"97acc3bf-1ed2-4ac9-8b22-73c912abc7bc"
+	.asciz	"410feece-7356-42b4-b7dd-c31fec7ebdeb"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
